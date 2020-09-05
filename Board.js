05a8c6563a1c7d33cdd4d1634b8003ee39a93a8e
dprebyl <drakeprebyl@gmail.com>
2020-09-05 00:01:02 -0500

    Basic class structure and board rendering

--- Board.js
+++ Board.js
@@ -1,16 +1,57 @@
 const ROWS = 10;
 const COLS = 10;
 
-// Initialize board
-window.addEventListener("DOMContentLoaded", () => {
-	let board = document.getElementById("board");
-	for (let row = ROWS; row >= 1; row--) {
-		let tr = document.createElement("tr");
-		for (let col = 1; col <= COLS; col++) {
-			let td = document.createElement("td");
-			td.innerText = "O"; // TODO: Create a space object here
-			tr.appendChild(td);
+class Board {
+	constructor(rows, cols) {
+		this.cells = [];
+		for (let row = 0; row < rows; row++) {
+			this.cells[row] = [];
+			for (let col = 0; col < cols; col++) {
+				this.cells[row][col] = new Space();
+			}
 		}
-		board.appendChild(tr);
 	}
-});
+	
+	/**
+	* @param table The DOM element to render the board to
+	* @param showShips Boolean for whether all ship locations should be visible
+	**/
+	render(table, showShips) {
+		table.innerHTML = ""; // Remove any existing cells
+		
+		// Add letter row
+		let letter = 'A';
+		let tr = document.createElement("tr");
+		let th = document.createElement("th");
+		tr.appendChild(th);
+		for (let cell of this.cells[0]) {
+			let th = document.createElement("th");
+			th.innerText = letter;
+			tr.appendChild(th);
+			letter = String.fromCharCode(letter.charCodeAt(0) + 1); // Increment letter
+		}
+		table.appendChild(tr);
+		
+		let num = 1;
+		for (let row of this.cells) {
+			let tr = document.createElement("tr");
+			
+			// Add number column
+			let th = document.createElement("th");
+			th.innerText = num;
+			tr.appendChild(th);
+			num++;
+			
+			for (let cell of row) {
+				let td = document.createElement("td");
+				if (cell.isHit) td.classList.add("hit");
+				if (cell.isHit || (showShips && cell.isShip)) td.classList.add("ship");
+				tr.appendChild(td);
+			}
+			table.appendChild(tr);
+		}
+	}
+	
+	checkWin() {
+	}
+}
---
+++ Space.js
@@ -0,0 +1,6 @@
+class Space {
+	constructor(cell) {
+		this.hasShip = false;
+		this.isHit = false;
+	}
+}
